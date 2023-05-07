a5e9380af337fe2fbe705af5816e829d305e502a
Diego-coder2525 <forgam2525@outlook.es>
2023-05-07 17:24:34 -0500

    Initial commit

---
+++ src/main/java/com/brydie/ProyectoOdioReactBckend/ProyectoOdioReactBckendApplication.java
@@ -0,0 +1,13 @@
+package com.brydie.ProyectoOdioReactBckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoOdioReactBckendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoOdioReactBckendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brydie/ProyectoOdioReactBckend/ProyectoOdioReactBckendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brydie.ProyectoOdioReactBckend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoOdioReactBckendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
