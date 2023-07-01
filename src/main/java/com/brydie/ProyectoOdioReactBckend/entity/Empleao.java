0fcb27c5a30ebc82d53a7ee572a54e560b72ce91
Bruno-xd <gonzalesramirezbruno@gmail.com>
2023-07-01 00:14:12 -0500

    entidad

---
+++ src/main/java/com/brydie/ProyectoOdioReactBckend/entity/Empleao.java
@@ -0,0 +1,75 @@
+package com.brydie.ProyectoOdioReactBckend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "empleados")
+public class Empleao {
+	
+	@Id
+	// PARA QUE SEA AUTOINCREMENTABLE
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// AGREGAMOS QUE NO SEA NULO, ETC
+	@Column(name = "nombre", length = 60, nullable = false)
+	private String nombre;
+
+	@Column(name = "apellido", length = 60, nullable = false)
+	private String apellido;
+
+	@Column(name = "email", length = 60, nullable = false, unique = true)
+	private String email;
+
+	// SIEMPRE SE GENERA UN CONSTRUCTOR VACIO EN LOS BEANS
+	public Empleao() {
+
+	}
+
+	public Empleao(Long id, String nombre, String apellido, String email) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
